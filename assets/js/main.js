108edf4109c112cdabdb5733273f7ad1ade58f35
“Laura <laureen.pelaez.isaza@gmail.com>
2017-05-23 22:03:41 -0400

    updated js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,15 @@
-//show up and dissapear
-
 window.onload = function() { //este evento hace que cuando la pagina se cargue se llamen las funciones
-    document.getElementById("start").addEventListener("click", mostrarCaja); //para interactuar los listeners
     document.getElementById("start").addEventListener("click", ocultarLista);
-    document.getElementById("phone").addEventListener("click", myFunction); //para interactuar los listeners
+    document.getElementById("box").addEventListener("click", mostrarCaja); //para interactuar los listeners
+    // document.getElementById("start").addEventListener("click", startGame);
 
+}
+
+function myFunction() {
+    alert("Thanks for signing up");
 
 }
 
-function ocultarLista() {
-    document.getElementsByClassName("start").classList.add('ocultar'); //con este busco nuevamente agregarle la propiedad para que se oculte 
-
-}
 
 function mostrarCaja() {
     console.log('works!');
@@ -19,6 +17,56 @@ function mostrarCaja() {
 
 }
 
-function myFunction() {
-    alert("Thanks for signing up");
+function ocultarLista() {
+    document.getElementById("box").classList.add('ocultar'); //con este busco nuevamente agregarle la propiedad para que se oculte 
+
 }
+
+/*Game
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
+
+document.addEventListener("keydown", dibujarTeclado);
+var fondo = document.getElementById("dibujar");
+var papel = fondo.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", x - 1, y - 1, x + 1, y + 1, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+function dibujarTeclado(evento) {
+    var colorcito = "#FAA";
+    var movimiento = 5;
+    switch (evento.keyCode) {
+        case teclas.UP:
+            dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+            y = y - movimiento;
+            break;
+        case teclas.DOWN:
+            dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+            y = y + movimiento;
+            break;
+        case teclas.LEFT:
+            dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+            x = x - movimiento;
+            break;
+        case teclas.RIGHT:
+            dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+            x = x + movimiento;
+            break;
+    }
+}*/
